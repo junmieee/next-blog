b4691e80c9a6e8f207f97ed99149d7d465e11b32
junmieee <junmieee@gmail.com>
2023-08-29 19:53:50 +0900

    p태그 변경

--- components/PostListItem.js
+++ components/PostListItem.js
@@ -8,8 +8,8 @@ export default function PostListItem({ post, tag }) {
     return (
         <div className=' w-full py-4 hover:drop-shadow-basic group'>
             <Link href={'/blog/' + post.slug} className="hover:drop-shadow-base">
-                <p className="text-2xl font-bold text-gray-900 dark:text-gray-100">{post.title}</p>
-                <p className="text-tertiary mt-1 text-gray-600 dark:text-gray-400">{post.description}</p>
+                <span className="text-2xl font-bold text-gray-900 dark:text-gray-100">{post.title}</span>
+                <span className="text-tertiary mt-1 text-gray-600 dark:text-gray-400">{post.description}</span>
             </Link>
             <div className="mt-2 inline-flex w-full items-start gap-2 text-sm">
                 <div className="flex flex-wrap items-center gap-2">
