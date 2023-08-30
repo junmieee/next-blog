39d0c34735fbe1c45c9b8eb71b8c987063e8ab2a
junmieee <junmieee@gmail.com>
2023-08-30 21:48:25 +0900

    dynamic imports 수정

--- pages/about.js
+++ pages/about.js
@@ -9,14 +9,23 @@ import dynamic from 'next/dynamic'
 
 const Code = dynamic(() =>
     import('react-notion-x/build/third-party/code').then((m) => m.Code)
+    , {
+        ssr: false
+    }
 )
 const Collection = dynamic(() =>
     import('react-notion-x/build/third-party/collection').then(
         (m) => m.Collection
-    )
+    ),
+    {
+        ssr: false
+    }
 )
 const Equation = dynamic(() =>
-    import('react-notion-x/build/third-party/equation').then((m) => m.Equation)
+    import('react-notion-x/build/third-party/equation').then((m) => m.Equation),
+    {
+        ssr: false
+    }
 )
 const Pdf = dynamic(
     () => import('react-notion-x/build/third-party/pdf').then((m) => m.Pdf),
