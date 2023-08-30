49d519a202afda5d9995f9e5af45b57948644ad0
junmieee <junmieee@gmail.com>
2023-08-30 20:39:00 +0900

    update

--- constants/dataset.js
+++ constants/dataset.js
@@ -21,15 +21,17 @@ export const allBlogPosts = allBlogs
     // .filter(isBlogPost)
     .map((post) => ({
         ...post,
-        dateValue: new Date(post.date).getTime()
+        // dateValue: new Date(post.date).getTime()
     }))
-    .sort((a, b) => b.dateValue - a.dateValue);
+// .sort((a, b) => b.dateValue - a.dateValue);
 
 
 export const allNotesPosts = allNotes.map((note) => ({
     ...note,
-    dateValue: new Date(note.date).getTime()
-})).sort((a, b) => b.dateValue - a.dateValue);
+    // dateValue: new Date(note.date).getTime()
+    // })).sort((a, b) => b.dateValue - a.dateValue);
+}));
+
 
 export const allBlogTags = Array.from(
     allBlogPosts.reduce((ac, v) => {
