f4c2775cbbc8674c896b6ad9133d0c5a2bf3bf01
junmieee <junmieee@gmail.com>
2023-08-30 21:25:06 +0900

    update

--- constants/dataset.js
+++ constants/dataset.js
@@ -17,33 +17,20 @@ export const allSeriesName = allBlogs
 //     ...note,
 // })).sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
-// export const allBlogPosts = allBlogs
-//     // .filter(isBlogPost)
-//     .map((post) => ({
-//         ...post,
-//         dateValue: new Date(post.date).getTime()
-//     }))
-//     .sort((a, b) => b.dateValue - a.dateValue);
-
 export const allBlogPosts = allBlogs
     // .filter(isBlogPost)
     .map((post) => ({
         ...post,
-        dateValue: parseDate(post.date).getTime(), // 직접 날짜 문자열을 파싱하여 dateValue 속성 생성
+        // dateValue: new Date(post.date).getTime()
     }))
-    .sort((a, b) => b.dateValue - a.dateValue);
+// .sort((a, b) => b.dateValue - a.dateValue);
 
-
-// export const allNotesPosts = allNotes.map((note) => ({
-//     ...note,
-//     dateValue: new Date(note.date).getTime()
-// })).sort((a, b) => b.dateValue - a.dateValue);
-// }));
 
 export const allNotesPosts = allNotes.map((note) => ({
     ...note,
-    dateValue: parseDate(note.date).getTime(), // 직접 날짜 문자열을 파싱하여 dateValue 속성 생성
-})).sort((a, b) => b.dateValue - a.dateValue);
+    // dateValue: new Date(note.date).getTime()
+    // })).sort((a, b) => b.dateValue - a.dateValue);
+}));
 
 
 export const allBlogTags = Array.from(
