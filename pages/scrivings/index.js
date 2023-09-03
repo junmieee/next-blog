4a5710b8a2f180708947a27c1dae81476dcf3ed7
junmieee <junmieee@gmail.com>
2023-09-03 19:27:43 +0900

    404 에러 수정

--- pages/scrivings/index.js
+++ pages/scrivings/index.js
@@ -7,7 +7,7 @@ import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { allNotes } from 'contentlayer/generated';
 import { allScrivTags } from 'constants/dataset';
-
+import { useRouter } from 'next/router';
 
 
 export const getStaticProps = async () => {
@@ -24,6 +24,7 @@ export default function Note({ notes, tags }) {
     const [selectedTag, setSelectedTag] = useState(null);
     const [sortedNotes, setSortedNotes] = useState([]);
     const [allTags, settags] = useState([]);
+    const router = useRouter()
 
     useEffect(() => {
         // const sorted = [...posts].sort((a, b) => b.dateValue - a.dateValue);
@@ -49,6 +50,9 @@ export default function Note({ notes, tags }) {
         setSelectedTag(null);
     };
 
+    const onLinkClick = (note) => {
+        router.push(`/scrivings/${note.slug}`)
+    }
 
     const filteredNotes = sortedNotes.filter((note) => {
         if (selectedTag === null) {
@@ -79,16 +83,18 @@ export default function Note({ notes, tags }) {
                 {filteredNotes.map((note, idx) => {
                     return (
                         <>
-                            <Link href={`/scrivings/${note.slug}`}   >
-                                <div key={idx} className="rounded-xl hover:drop-shadow-basic dark:bg-darkPrimary flex transform justify-center gap-4 rounded-sm border border-6 border-gray-300 p-4 dark:border-neutral-700 hover:dark:bg-darkSecondary "
-                                >
-                                    <div className=" transition group-hover:scale-110 sm:group-hover:scale-100 dark:text-gray-100 text-gray-600  ">
-                                        <span className="ml-2 text-sm text-xl font-bold">{note.title}</span>
-                                    </div>
-                                    <p className=" select-none text-sm font-semibold sm:inline-flex md:text-base">
-                                    </p>
+                            {/* <Link href={`/scrivings/${note.slug}`}   > */}
+                            <div
+                                onClick={() => onLinkClick(note)}
+                                key={idx} className="rounded-xl hover:drop-shadow-basic dark:bg-darkPrimary flex transform justify-center gap-4 rounded-sm border border-6 border-gray-300 p-4 dark:border-neutral-700 hover:dark:bg-darkSecondary "
+                            >
+                                <div className=" transition group-hover:scale-110 sm:group-hover:scale-100 dark:text-gray-100 text-gray-600  ">
+                                    <span className="ml-2 text-sm text-xl font-bold">{note.title}</span>
                                 </div>
-                            </Link>
+                                <p className=" select-none text-sm font-semibold sm:inline-flex md:text-base">
+                                </p>
+                            </div>
+                            {/* </Link> */}
                         </>
                     )
                 })}
