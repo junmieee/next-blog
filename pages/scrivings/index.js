40b1be0ea2797c5bac1895cd75b69b964057b886
junmieee <junmieee@gmail.com>
2023-09-05 00:23:19 +0900

    update

--- pages/scrivings/index.js
+++ pages/scrivings/index.js
@@ -23,7 +23,7 @@ export default function Note({ notes, tags }) {
     const [selectedTag, setSelectedTag] = useState(null);
     const [sortedNotes, setSortedNotes] = useState([]);
     const [allTags, settags] = useState([]);
-    const router = useRouter()
+    // const router = useRouter()
     useEffect(() => {
         const sorted = notes.sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)))
         setSortedNotes(sorted);
@@ -61,39 +61,36 @@ export default function Note({ notes, tags }) {
 
     return (
         <>
-            <Suspense>
-                <div className='flex pt-6 items-end'>
-                    <Title title="Scrivings" />
-                </div>
-                <div className="mt-6 flex flex-wrap gap-2 py-4">
-                    <Tag tag="All" onClick={onAllTagClick} selected={selectedTag === null} />
-                    {allTags.map((tag, i) => (
-                        <Tag key={i} tag={tag} onClick={() => onTagClick(tag)} selected={selectedTag === tag} />
-                    ))}
-                </div>
-                <motion.div
-                    initial="hidden"
-                    whileInView="visible"
-                    variants={FadeContainer}
-                    viewport={{ once: true }}
-                    className="my-10 grid grid-cols-2 gap-4 mt-14 "
-                >
-                    {filteredNotes.map((note, idx) => (
-                        // <Link href={`/scrivings/` + note.slug} as={"/scrivings/" + note.slug} >
-                        <div
-                            onClick={() => onLinkClick(note)}
-                            key={idx} className="rounded-xl hover:drop-shadow-basic dark:bg-darkPrimary flex transform justify-center gap-4 rounded-sm border border-6 border-gray-300 p-4 dark:border-neutral-700 hover:dark:bg-darkSecondary "
-                        >
-                            <div className=" transition group-hover:scale-110 sm:group-hover:scale-100 dark:text-gray-100 text-gray-600  ">
-                                <span className="ml-2 text-sm text-xl font-bold">{note.title}</span>
+            <div className='flex pt-6 items-end'>
+                <Title title="Scrivings" />
+            </div>
+            <div className="mt-6 flex flex-wrap gap-2 py-4">
+                <Tag tag="All" onClick={onAllTagClick} selected={selectedTag === null} />
+                {allTags.map((tag, i) => (
+                    <Tag key={i} tag={tag} onClick={() => onTagClick(tag)} selected={selectedTag === tag} />
+                ))}
+            </div>
+            <motion.div
+                initial="hidden"
+                whileInView="visible"
+                variants={FadeContainer}
+                viewport={{ once: true }}
+                className="my-10 grid grid-cols-2 gap-4 mt-14 "
+            >
+                <Suspense>
+                    {filteredNotes.map((note) => (
+                        <Link key={note.id} href={`/scrivings?note.slug=${note.slug}`} as={`/scrivings/${note.slug}`}>
+                            <div className="rounded-xl hover:drop-shadow-basic dark:bg-darkPrimary flex transform justify-center gap-4 rounded-sm border border-6 border-gray-300 p-4 dark:border-neutral-700 hover:dark:bg-darkSecondary">
+                                <div className="transition group-hover:scale-110 sm:group-hover:scale-100 dark:text-gray-100 text-gray-600">
+                                    <span className="ml-2 text-sm text-xl font-bold">{note.title}</span>
+                                </div>
+                                <p className="select-none text-sm font-semibold sm:inline-flex md:text-base"></p>
                             </div>
-                            <p className=" select-none text-sm font-semibold sm:inline-flex md:text-base">
-                            </p>
-                        </div>
-                        // </Link>
+                        </Link>
                     ))}
-                </motion.div>
-            </Suspense>
+                </Suspense>
+
+            </motion.div>
 
         </>
 
