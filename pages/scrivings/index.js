321cecb1b15dc8d61f8dae2221a2d8456c578ef3
junmieee <junmieee@gmail.com>
2023-10-22 20:51:56 +0900

    scriving 수정

--- pages/scrivings/index.js
+++ pages/scrivings/index.js
@@ -79,7 +79,7 @@ export default function Note({ notes, tags }) {
             >
                 {/* <Suspense> */}
                 {filteredNotes.map((note, idx) => (
-                    <Link key={idx} href={`/scrivings/${note.slug}`} as={`/scrivings/${note.slug}`}>
+                    <Link key={idx} href={'/scrivings/' + note.slug} as={'/scrivings/' + note.slug}  >
                         <div className="rounded-xl hover:drop-shadow-basic dark:bg-darkPrimary flex transform justify-center gap-4 rounded-sm border border-6 border-gray-300 p-4 dark:border-neutral-700 hover:dark:bg-darkSecondary">
                             <div className="transition group-hover:scale-110 sm:group-hover:scale-100 dark:text-gray-100 text-gray-600">
                                 <span className="ml-2 text-sm text-xl font-bold">{note.title}</span>
